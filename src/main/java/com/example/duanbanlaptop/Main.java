c6dea6e9642f1beff656e6ecf2c0c789a7707b74
Viet Tam <viettam205@gmail.com>
2024-10-22 15:21:34 +0700

    sulidangki

--- src/main/java/com/example/duanbanlaptop/Main.java
+++ src/main/java/com/example/duanbanlaptop/Main.java
@@ -1,24 +1,27 @@
 package com.example.duanbanlaptop;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 import java.sql.SQLException;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("login/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("signup/signup.fxml"));
         stage.setTitle("login");
         stage.setScene(new Scene(root));
         stage.show();
     }
 
-    public static void main(String[] args) throws SQLException {
+    public static  void main(String[] args) throws SQLException {
        launch(args);
+
+
     }
+
+
 }
--- src/main/java/com/example/duanbanlaptop/function/SignupFuncion.java
+++ src/main/java/com/example/duanbanlaptop/function/SignupFuncion.java
@@ -1,4 +1,108 @@
 package com.example.duanbanlaptop.function;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.example.duanbanlaptop.Connect;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class SignupFuncion {
-}
+
+
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private TextField txtPassword;
+    @FXML
+    private TextField txtPhoneNumber;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private TextField txtAddress;
+    @FXML
+    private ChoiceBox<String> txtGender;
+    @FXML
+    private Label error;
+
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    @FXML
+    private void signup() throws SQLException {
+        String username = txtUsername.getText();
+        String password = txtPassword.getText();
+        String phonenumber = txtPhoneNumber.getText();
+        String email = txtEmail.getText();
+        String address = txtAddress.getText();
+        String gender = txtGender.getValue();
+
+
+        if (username.isEmpty() || password.isEmpty() || phonenumber.isEmpty() || email.isEmpty() || address.isEmpty() || gender == null) {
+            error.setText("Please fill in all registration information !");
+        } else {
+
+
+            for (char t : username.toCharArray()) {
+                if (!Character.isLetterOrDigit(t)) {
+                    error.setText("Username cannot contain special characters.");
+                    return;
+                }
+            }
+
+
+            if (password.length() < 8) {
+                error.setText("Password must be at least 8 characters long.");
+            }
+
+            else if (phonenumber.length() < 10 || phonenumber.length() > 10) {
+                error.setText("Phone numbers must be 10 characters ");
+                for (char t : phonenumber.toCharArray()) {
+                    if (!Character.isDigit(t)) {
+                        error.setText("Phone numbers must be number ");
+                        return;
+                    }
+                }
+            }else{
+                Connect connect = new Connect();
+                Connection conn = connect.connect();
+
+                String query = "insert into  user  (username, password, email, phoneNumber, role, address, gender) VALUES (?, ?, ?, ?, ?, ?, ?)";
+                PreparedStatement add = conn.prepareStatement(query);
+                add.setString(1, username);
+                add.setString(2, password);
+                add.setString(3, email);
+                add.setString(4, phonenumber);
+                add.setString(5, "customer");
+                add.setString(6, address);
+                add.setString(7, gender);
+                int row = add.executeUpdate();
+                if (row != 0) {
+                    System.out.println("thanh cong");
+                }
+
+            }
+                }
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
