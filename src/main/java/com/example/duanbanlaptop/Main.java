6719956b40579ee6c4f1782a007899fc57d80ff7
vandan <levandanhh2@gmail.com>
2024-10-22 15:27:29 +0700

    chucnangchuyencanh

--- src/main/java/com/example/duanbanlaptop/Main.java
+++ src/main/java/com/example/duanbanlaptop/Main.java
@@ -1,7 +1,9 @@
 package com.example.duanbanlaptop;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,13 +12,17 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 public class Main extends Application {
+    public static Stage stage;
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("login/login.fxml"));
+        this.stage = stage;
+        Parent fxmlLoader = FXMLLoader.load(getClass().getResource("view/login.fxml"));
+        Scene scene = new Scene(fxmlLoader);
         stage.setTitle("login");
-        stage.setScene(new Scene(root));
+        stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) throws SQLException {
        launch(args);
