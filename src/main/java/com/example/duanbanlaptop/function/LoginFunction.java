a3c44d45a6702c42de067ede68005ecf3a030549
vandan <levandanhh2@gmail.com>
2024-10-23 22:20:45 +0700

    thaydoiphandangnhap

--- src/main/java/com/example/duanbanlaptop/function/LoginFunction.java
+++ src/main/java/com/example/duanbanlaptop/function/LoginFunction.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -58,7 +59,12 @@ public class LoginFunction {
                 String role = resultSet.getString("role");
                 System.out.println("Thanh vien dang nhap thanh cong co id la : " + id + " va vai tro la : " + role);
             }else {
-                System.out.println("Dang nhap that bai");
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setContentText("Incorrect password or username!");
+                alert.showAndWait();
+                txtUsername.setText("");
+                txtPassword.setText("");
+                bug.setText("");
             }
         }
     }
