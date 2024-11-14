923f4d766301ca63f4be721bb042f3084f6855db
Viet Tam <viettam205@gmail.com>
2024-11-14 15:40:25 +0700

    update

--- src/main/java/com/example/duanbanlaptop/Connect.java
+++ src/main/java/com/example/duanbanlaptop/Connect.java
@@ -4,7 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Connect {
+public class
+Connect {
     private String url = "jdbc:mysql://localhost:3306/duanbanhang";
     private String usernameDB = System.getenv("usernameDB");
     private String passwordDB = System.getenv("passwordDB");
